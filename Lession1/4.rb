1488031ec6301f5d7e393b68622a942d1ac8bf38
noo2bf <105634361+noo2bf@users.noreply.github.com>
2022-05-19 09:26:25 +0300

    Create 4.rb

---
+++ Lession1/4.rb
@@ -0,0 +1,23 @@
+print "Введите коффициент a: "
+a = Integer(gets.chomp)
+
+print "Введите коффициент b: "
+b = Integer(gets.chomp)
+
+print "Введите коффициент c: "
+c = Integer(gets.chomp)
+
+dic = (b**2-(4*a*c))
+
+if dic < 0
+	puts "Корней нет"
+elsif dic == 0
+	x1= (-b+Math.sqrt(dic))/(2*a)
+	puts "Дискриминант равен #{dic}, корень равен #{x1}"
+elsif dic > 0
+	x1= (-b+Math.sqrt(dic))/(2*a)
+    x2 =(-b-Math.sqrt(dic))/(2*a)
+    puts "Дискриминант равен #{dic}, корень равен #{x1}, корень равен #{x2}"
+
+end
+
