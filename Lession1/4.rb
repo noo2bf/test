735ca06578a76f6876f8a1cfa0f563d8a4b9bbb2
noo2bf <105634361+noo2bf@users.noreply.github.com>
2022-05-23 08:02:12 +0300

    Update 4.rb

--- Lession1/4.rb
+++ Lession1/4.rb
@@ -7,17 +7,16 @@ b = Integer(gets.chomp)
 print "Введите коффициент c: "
 c = Integer(gets.chomp)
 
-dic = (b**2-(4*a*c))
+dic = (b**2 - (4 * a * c))
 
 if dic < 0
-	puts "Корней нет"
+  puts "Корней нет"
 elsif dic == 0
-	x1= (-b+Math.sqrt(dic))/(2*a)
-	puts "Дискриминант равен #{dic}, корень равен #{x1}"
+  x1 = (-b + Math.sqrt(dic)) / (2 * a)
+  puts "Дискриминант равен #{dic}, корень равен #{x1}"
 elsif dic > 0
-	x1= (-b+Math.sqrt(dic))/(2*a)
-    x2 =(-b-Math.sqrt(dic))/(2*a)
-    puts "Дискриминант равен #{dic}, корень равен #{x1}, корень равен #{x2}"
-
+  x1 = (-b + Math.sqrt(dic)) / (2 * a)
+  x2 =(-b - Math.sqrt(dic)) / (2 * a)
+  puts "Дискриминант равен #{dic}, корень равен #{x1}, корень равен #{x2}"
 end
 
