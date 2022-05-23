5cfe6b022ad657a786c9a460723a6a6355bbc2b4
noo2bf <105634361+noo2bf@users.noreply.github.com>
2022-05-23 07:52:19 +0300

    Update 2.rb

--- Lession1/2.rb
+++ Lession1/2.rb
@@ -1,6 +1,6 @@
 print "Введите размер основания треугольника "
 a = Integer(gets.chomp)
 print "Введите высоту треугольника "
-b = Integer(gets.chomp)
-Plo = (1/2*a*b)
-puts "Прощадь треугольника равна #{c}"
+h = Integer(gets.chomp)
+square = (( a * h ) / 2 ) 
+puts "Прощадь треугольника равна #{square}"
