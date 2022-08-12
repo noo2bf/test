df32a77728a261bb84e45bd9787ffcfd46fdaac6
noo2bf <105634361+noo2bf@users.noreply.github.com>
2022-08-12 18:36:22 +0300

    delete

--- Lession1/2.rb
+++
@@ -1,6 +0,0 @@
-print "Введите размер основания треугольника "
-a = Integer(gets.chomp)
-print "Введите высоту треугольника "
-h = Integer(gets.chomp)
-square = (( a * h ) / 2 ) 
-puts "Прощадь треугольника равна #{square}"
