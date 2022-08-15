4b61bcba9af072b71008fabf341cf2e538583df2
noo2bf <105634361+noo2bf@users.noreply.github.com>
2022-08-15 19:49:26 +0300

    delete

---
+++ Lession 1/app1.rb
@@ -0,0 +1,13 @@
+
+print "Напишите своё имя :"
+name = gets.chomp
+
+print "Напишите свой рост "
+growth = Integer(gets.chomp)
+
+ideal_weight = (growth - 100)*1.15
+if ideal_weight >=0
+  puts "#{name} ваш идеальный вес #{ideal_weight}"
+else
+  puts "Ваш вес уже оптимальный"
+end
